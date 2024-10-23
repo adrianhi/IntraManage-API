a485a4252c824ea1b187bdd62f7535d493fc0bcc
Adrian Hidalgo <hidalgobeltreadrian@gmail.com>
2024-10-23 18:46:21 -0400

    Add project files.

---
+++ IntraManage/Data/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntraManage.Data.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public string Cedula { get; set; } = null!;
+
+    public int? RoleId { get; set; }
+
+    public int? DepartmentId { get; set; }
+
+    public virtual Department? Department { get; set; }
+
+    public virtual Role? Role { get; set; }
+}
---
+++ IntraManage/Data/Models/IntraManageContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntraManage.Data.Models;
+
+public partial class IntraManageContext : DbContext
+{
+    public IntraManageContext()
+    {
+    }
+
+    public IntraManageContext(DbContextOptions<IntraManageContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Departme__3214EC07B4A34475");
+
+            entity.Property(e => e.DepartmentName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Employee__3214EC07003DB3D1");
+
+            entity.HasIndex(e => e.Email, "UQ__Employee__A9D105348E7FA80D").IsUnique();
+
+            entity.Property(e => e.Cedula)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.DepartmentId).HasColumnName("DepartmentID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.PasswordHash)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.RoleId).HasColumnName("RoleID");
+
+            entity.HasOne(d => d.Department).WithMany(p => p.Employees)
+                .HasForeignKey(d => d.DepartmentId)
+                .HasConstraintName("FK__Employees__Depar__3D5E1FD2");
+
+            entity.HasOne(d => d.Role).WithMany(p => p.Employees)
+                .HasForeignKey(d => d.RoleId)
+                .HasConstraintName("FK__Employees__RoleI__3C69FB99");
+        });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Roles__3214EC07B10855EF");
+
+            entity.Property(e => e.RoleName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
